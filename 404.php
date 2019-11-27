22e1d3f44b871e96f342dfbb177dc6efe1722a66
Baris Karaer <bariskaraerx@gmail.com>
2019-11-27 16:06:35 +0300

    Update 404.php
    Updated 404 PHP based on the project of HPMS.

--- 404.php
+++ 404.php
@@ -15,42 +15,29 @@ include 'inc/function.php';
 if(empty($r1)){$in="index.php";}
 	else
 	{
-	if($r1=='hizmetlerimiz') { $in='hizmetlerimiz.php'; }
 	
-	
-
-	
-	elseif($r1=='json')  {$in='json/'.$r2.'.php';}
 	
 
 		//** Client Side **//
 		
 		
-	elseif($r1=='cikis') { $in='minor/cikis.php'; }
-	elseif($r1=='giris') { $in='minor/giris.php'; }
-	elseif($r1=='anasayfa')	{$in='minor/anasayfa.php';}
+	if($r1=='logout') { $in='minor/logout.php'; }
+	elseif($r1=='login') { $in='minor/login.php'; }
+	elseif($r1=='home')	{$in='minor/home.php';}
 
 	elseif($r1=='patients')  {$in='minor/modul/books/'.$r2.'.php';}
-	elseif($r1=='students')  {$in='minor/modul/students/'.$r2.'.php';}
+	elseif($r1=='complaints')  {$in='minor/modul/complaints/'.$r2.'.php';}
+	elseif($r1=='appointments')  {$in='minor/modul/appointments/'.$r2.'.php';}
+	elseif($r1=='admin')  {$in='minor/modul/admin/'.$r2.'.php';}
 
-	elseif($r1=='interview')  {$in='minor/modul/interview/'.$r2.'.php';}
-	elseif($r1=='dynamic')  {$in='minor/modul/dynamic/'.$r2.'.php';}
-	elseif($r1=='users')  {$in='minor/modul/users/'.$r2.'.php';}
-	elseif($r1=='admins')  {$in='minor/modul/admin/'.$r2.'.php';}
-	elseif($r1=='branch')  {$in='minor/modul/branch/'.$r2.'.php';}
-	elseif($r1=='chat')  {$in='minor/modul/chat/'.$r2.'.php';}
-	elseif($r1=='pool')  {$in='minor/modul/pool/'.$r2.'.php';}
-	elseif($r1=='setting')  {$in='minor/modul/settings/'.$r2.'.php';}
-	elseif($r1=='sue')  {$in='minor/modul/sue/'.$r2.'.php';}
-	elseif($r1=='notification')  {$in='minor/modul/notification/'.$r2.'.php';}
-	elseif($r1=='appointment')  {$in='minor/modul/appointment/'.$r2.'.php';}
+
 	
 	
 	
 	
 	//** Main Admin **//
 		
-		
+		/*
 	elseif($r1=='logout') { $in='major/logout.php'; }
 	elseif($r1=='login') { $in='major/login.php'; }
 	elseif($r1=='home')	{$in='major/home.php';}
@@ -60,7 +47,7 @@ if(empty($r1)){$in="index.php";}
 
 	elseif($r1=='interview')  {$in='major/modul/interview/'.$r2.'.php';}
 	elseif($r1=='dynamic')  {$in='major/modul/dynamic/'.$r2.'.php';}
-	
+		*/
 
 
 }
