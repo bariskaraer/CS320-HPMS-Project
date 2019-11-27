e259e3b34f9d00234579a77fcbd9f7cfbf2a79a9
Rup <erdem.gonul@ozu.edu.tr>
2019-11-27 03:44:59 +0300

    login page modified,global script file created,global stylesheet for substylesheets created

--- minor/giris.php
+++ minor/giris.php
@@ -1,181 +1,156 @@
-<?
-   if ($_POST){
-Header ("Location: $url/anasayfa");
 
-	$email	= sc($_POST["email"]);
-	$password	= sc($_POST["password"]);
-
-	$query = $db->prepare("SELECT * FROM admins WHERE adminEmail = ? and adminPassword=?  ");
-	$query->execute(array($email,$password));
-	if($row = $query->fetch())
-	{
-
-			$_SESSION['adminID'] = $row['adminID'];
-			$visitDate = $db->prepare("UPDATE admins SET visitDate = ? WHERE adminID = ?");
-			$visitDate->execute(array(date("Y/m/d G:i:s"), $row['adminID']));
-			Header ("Location: $url/anasayfa");
-
-
-
-
-	}
-	else
-	{
-		$errorr= 'Geçersiz E-posta / Şifre.';
-		if (isset($errorr)){setcookie("errorr",$errorr,time()+3);}
-		Header ("Location: $url/giris");
-	}
-
-
-
-
-   }
-
-if (isset($error))
-{
-	setcookie("error",$error,time()+2);
-	Header ("Location: ".$_SERVER['HTTP_REFERER']);
-}
-
-
+<?php
+    include('bootstrap.php');
+    $url=BASE_URL;
 ?>
-<!DOCTYPE html>
+<head>
+<link href="<?php echo $url; ?>/linkrel.php" rel="stylesheet" type="text/css" />
+</head>
+
+    <body  class="kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header-mobile--fixed kt-subheader--enabled kt-subheader--fixed kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading"  >
 
 
-<html lang="en">
+    	<!-- begin:: Page -->
+              	<div class="kt-grid kt-grid--ver kt-grid--root">
+              		<div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v1" id="kt_login">
+              	<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
+              		<!--begin::Aside-->
+              		<div class="kt-grid__item kt-grid__item--order-tablet-and-mobile-2 kt-grid kt-grid--hor kt-login__aside" style="background-image: url(<?php echo $url; ?>/assets/media//bg/bg-4.jpg);">
+              			<div class="kt-grid__item">
+              				<a href="#" class="kt-login__logo">
+              					<img src="<?php echo $url; ?>/assets/media/logos/logo-4.png">
+              				</a>
+              			</div>
+              			<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver">
+              				<div class="kt-grid__item kt-grid__item--middle">
+              					<h3 class="kt-login__title">HPMS-PROJECT</h3>
+              					<h4 class="kt-login__subtitle">This is a group project for CS320. Our project is called HPMS(Hospital Patient Management System). The names of the participants (coders) are Barış Karaer, Mustafa Alper Sayan and Erdem Gönül.</h4>
+              				</div>
+              			</div>
+              		</div>
+              		<!--begin::Aside-->
 
-	<!-- begin::Head -->
-	<head>
-		<meta charset="utf-8" />
-		<title>Metronic | Login Page v2</title>
-		<meta name="description" content="Login page example">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+              		<!--begin::Content-->
+              		<div class="kt-grid__item kt-grid__item--fluid  kt-grid__item--order-tablet-and-mobile-1  kt-login__wrapper">
+              			<!--begin::Head-->
+              			<div class="kt-login__head">
+              				<span class="kt-login__signup-label">Don't have an account yet?</span>&nbsp;&nbsp;
+              				<a href="#" class="kt-link kt-login__signup-link">Sign Up!</a>
+              			</div>
+              			<!--end::Head-->
 
-		<!--begin::Fonts -->
-		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-		<script>
-			WebFont.load({
-                google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
-                active: function() {
-                    sessionStorage.fonts = true;
-                }
-            });
-        </script>
+              			<!--begin::Body-->
+              			<div class="kt-login__body">
 
-		<!--end::Fonts -->
+              				<!--begin::Signin-->
+              				<div class="kt-login__form">
+              					<div class="kt-login__title">
+              						<h3>Sign In</h3>
+              					</div>
 
-		<!--begin::Page Custom Styles(used by this page) -->
-		<link href="<? echo $url ?>/assets/app/custom/login/login-v2.default.css" rel="stylesheet" type="text/css" />
+              					<!--begin::Form-->
+              					<form class="kt-form" action="" novalidate="novalidate">
+              						<div class="form-group">
+              							<input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+              						</div>
+              						<div class="form-group">
+              							<input class="form-control" type="password" placeholder="Password" name="password">
+              						</div>
+              						<!--begin::Action-->
+              						<div class="kt-login__actions">
+              							<a href="#" class="kt-link kt-login__link-forgot">
+              								Forgot Password ?
+              							</a>
+              							<button id="kt_login_signin_submit" class="btn btn-primary btn-elevate kt-login__btn-primary">Sign In</button>
+              						</div>
+              						<!--end::Action-->
+              					</form>
+              					<!--end::Form-->
 
-		<!--end::Page Custom Styles -->
+              					<!--begin::Divider-->
+              					<div class="kt-login__divider">
+              						<div class="kt-divider">
+              							<span></span>
+              							<span>OR</span>
+              							<span></span>
+              						</div>
+              					</div>
+              					<!--end::Divider-->
 
-		<!--begin:: Global Mandatory Vendors -->
-		<link href="<? echo $url ?>/assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
+              					<!--begin::Options-->
+              					<div class="kt-login__options">
+              						<a href="#" class="btn btn-primary kt-btn">
+              							<i class="fab fa-facebook-f"></i>
+              							Facebook
+              						</a>
 
+              						<a href="#" class="btn btn-info kt-btn">
+              							<i class="fab fa-twitter"></i>
+              							Twitter
+              						</a>
 
-
-		<!--begin::Global Theme Styles(used by all pages) -->
-		<link href="<? echo $url ?>/assets/demo/default/base/style.bundle.css" rel="stylesheet" type="text/css" />
-
-		<!--end::Global Theme Styles -->
-
-		<!--begin::Layout Skins(used by all pages) -->
-		<link href="<? echo $url ?>/assets/demo/default/skins/header/base/light.css" rel="stylesheet" type="text/css" />
-		<link href="<? echo $url ?>/assets/demo/default/skins/header/menu/light.css" rel="stylesheet" type="text/css" />
-		<link href="<? echo $url ?>/assets/demo/default/skins/brand/dark.css" rel="stylesheet" type="text/css" />
-		<link href="<? echo $url ?>/assets/demo/default/skins/aside/dark.css" rel="stylesheet" type="text/css" />
-
-		<!--end::Layout Skins -->
-		<link rel="shortcut icon" href="<? echo $url ?>/assets/media/logos/favicon.ico" />
-	</head>
-
-	<!-- end::Head -->
-
-	<!-- begin::Body -->
-	<body class="kt-header--fixed kt-header-mobile--fixed kt-subheader--fixed kt-subheader--enabled kt-subheader--solid kt-aside--enabled kt-aside--fixed kt-page--loading">
-
-		<!-- begin:: Page -->
-		<div class="kt-grid kt-grid--ver kt-grid--root">
-			<div class="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v2 kt-login--signin" id="kt_login">
-				<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" style="background-image: url(<? echo $url ?>/assets/media//bg/bg-1.jpg);">
-					<div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
-						<div class="kt-login__container">
-							<div class="kt-login__logo">
-								<a href="#">
-									<img src="<? echo $url ?>/assets/media/logos/logo-mini-2-md.png">
-								</a>
-							</div>
-							<div class="kt-login__signin">
-								<div class="kt-login__head">
-									<h3 class="kt-login__title">Sign In To Admin</h3>
-								</div>
-									 <?php if (isset($_COOKIE["error"]))	{echo alertError($_COOKIE["error"]);}?>
-                                    <?php if (isset($_COOKIE["msg"]))	{echo alertMsg($_COOKIE["msg"]);}?>
-                                    <form   method="post" class="kt-form" name="login" onsubmit="return " >
-									<div class="input-group">
-										<input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
-									</div>
-									<div class="input-group">
-										<input class="form-control" type="password" placeholder="Password" name="password">
-									</div>
-									
-									<div class="kt-login__actions">
-										<button id="kt_login_signin_submit" class="btn btn-pill kt-login__btn-primary">Sign In</button>
-									</div>
-								</form>
-							</div>
-							
-							
-							
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- end:: Page -->
-
-		<!-- begin::Global Config(global config for global JS sciprts) -->
-		<script>
-			var KTAppOptions = {
-				"colors": {
-					"state": {
-						"brand": "#5d78ff",
-						"dark": "#282a3c",
-						"light": "#ffffff",
-						"primary": "#5867dd",
-						"success": "#34bfa3",
-						"info": "#36a3f7",
-						"warning": "#ffb822",
-						"danger": "#fd3995"
-					},
-					"base": {
-						"label": ["#c5cbe3", "#a1a8c3", "#3d4465", "#3e4466"],
-						"shape": ["#f0f3ff", "#d9dffa", "#afb4d4", "#646c9a"]
-					}
-				}
-			};
-		</script>
-
-		<!-- end::Global Config -->
+              						<a href="#" class="btn btn-danger kt-btn">
+              							<i class="fab fa-google"></i>
+              							Google
+              						</a>
+              					</div>
+              					<!--end::Options-->
+              				</div>
+              				<!--end::Signin-->
+              			</div>
+              			<!--end::Body-->
+              		</div>
+              		<!--end::Content-->
+              	</div>
+              </div>
 
 
 
-		<!--begin::Global Theme Bundle(used by all pages) -->
-		<script src="<? echo $url ?>/assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
+              </div>
+              <script src="<?php echo $url; ?>/assets/js/demo1/pages/login/login-1.js" type="text/javascript"></script>
+              <script src="<?php echo $url; ?>/assets/js/demo1/scripts.bundle.js" type="text/javascript"></script>
+              <script src="<?php echo $url; ?>/assets/js/demo1/pages/login/login-1.js" type="text/javascript"></script>
+</body>
 
-		<!--end::Global Theme Bundle -->
 
-		<!--begin::Page Scripts(used by this page) -->
-		<script src="<? echo $url ?>/assets/app/custom/login/login-general.js" type="text/javascript"></script>
 
-		<!--end::Page Scripts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Roboto:300,400,500,600,700">        <!--end::Fonts -->
+    <link href="<?php echo $url; ?>/assets/css/demo1/pages/login/login-1.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
 
-		<!--begin::Global App Bundle(used by all pages) -->
-		<script src="<? echo $url ?>/assets/app/bundle/app.bundle.js" type="text/javascript"></script>
 
-		<!--end::Global App Bundle -->
-	</body>
-
-	<!-- end::Body -->
+    <link href="<?php echo $url; ?>/assets/vendors/general/tether/dist/css/tether.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-datetime-picker/css/bootstrap-datetimepicker.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-timepicker/css/bootstrap-timepicker.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/select2/dist/css/select2.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/ion-rangeslider/css/ion.rangeSlider.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/nouislider/distribute/nouislider.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/owl.carousel/dist/assets/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/owl.carousel/dist/assets/owl.theme.default.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/dropzone/dist/dropzone.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/quill/dist/quill.snow.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/@yaireo/tagify/dist/tagify.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/summernote/dist/summernote.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/bootstrap-markdown/css/bootstrap-markdown.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/animate.css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/toastr/build/toastr.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/dual-listbox/dist/dual-listbox.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/morris.js/morris.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/sweetalert2/dist/sweetalert2.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/socicon/css/socicon.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/custom/vendors/line-awesome/css/line-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/custom/vendors/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/custom/vendors/flaticon2/flaticon.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/vendors/general/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/css/demo1/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/css/demo1/skins/header/base/light.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/css/demo1/skins/header/menu/light.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/css/demo1/skins/brand/dark.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url; ?>/assets/css/demo1/skins/aside/dark.css" rel="stylesheet" type="text/css" />        <!--end::Layout Skins -->
+    <link rel="shortcut icon" href="<?php echo $url; ?>/assets/media/logos/favicon.ico" />
 </html>
