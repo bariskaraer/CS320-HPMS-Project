46e5139728b191a24c86086c5ee192a6df0d606d
Baris Karaer <bariskaraerx@gmail.com>
2019-12-05 18:09:43 +0300

    Footer Update

--- minor/footer.php
+++ minor/footer.php
@@ -1,10 +1,10 @@
 <div class="kt-footer kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
 						<div class="kt-footer__copyright">
-							2019&nbsp;&copy;&nbsp;<a href="http://krakersoft.com/" target="_blank" class="kt-link">Krakersoft</a>
+							2019&nbsp;&copy;&nbsp;<a href="" target="_blank" class="kt-link"></a>
 						</div>
 						<div class="kt-footer__menu">
-							<a href="http://krakersoft.com/" target="_blank" class="kt-footer__menu-link kt-link">Hakkımızda</a>
-							<a href="http://krakersoft.com/" target="_blank" class="kt-footer__menu-link kt-link">Ürünlerimiz</a>
-							<a href="http://krakersoft.com/" target="_blank" class="kt-footer__menu-link kt-link">İletişim</a>
+							<a href="" target="_blank" class="kt-footer__menu-link kt-link"></a>
+							<a href="" target="_blank" class="kt-footer__menu-link kt-link"></a>
+							<a href="" target="_blank" class="kt-footer__menu-link kt-link"></a>
 						</div>
 					</div>
