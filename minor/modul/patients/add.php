3f6b75dec8e63e6d35f7eed62cfd49a0c61c0a82
Baris Karaer <bariskaraerx@gmail.com>
2019-12-05 18:19:37 +0300

    Update add.php

--- minor/modul/patients/add.php
+++ minor/modul/patients/add.php
@@ -1,4 +1,4 @@
-<?php if (empty($_SESSION['course_adminID'])){Header ("Location: ".$url."/login");}?>
+<?php if (empty($_SESSION['adminID'])){Header ("Location: ".$url."/login");}?>
  <?
 	
 if ($_POST){
