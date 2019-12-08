e5772ade0c4727909425f34565938a82d9ff6b66
Baris Karaer <bariskaraerx@gmail.com>
2019-12-08 12:58:18 +0300

    List To do

--- minor/modul/patients/list.php
+++ minor/modul/patients/list.php
@@ -1,18 +1,7 @@
 <?php if (empty($_SESSION['adminID'])){Header ("Location: ".$url."/login");}?>
 <!DOCTYPE html>
 
-<!-- 
-Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4 & Angular 7
-Author: KeenThemes
-Website: http://www.keenthemes.com/
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
-License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
--->
+
 <html lang="en">
 
 	<!-- begin::Head -->
@@ -123,7 +112,8 @@ License: You must have a valid license purchased only from themeforest(the above
 																<th>Age</th>
 																<th>Treatment Plan</th>
 																<th>Complaints</th>
-																<th>Action</th>
+																<th>Appointments</th>
+																<th>Delete</th>
 																
 																										
 															</tr>
@@ -150,9 +140,9 @@ License: You must have a valid license purchased only from themeforest(the above
 																<td><? echo $patientName ?> <? echo $patientSurname ?></td>
 																<td ><? echo $patientAge ?></td>
 																<td ><? echo $patientTreatmentPlan ?></td>
-																<td ><? echo $patientComplaints ?></td>
-																
-																<td >delete</td>
+																<td >link for patient Complaints</td>
+																<td >patient Appointment Link</td>
+																<td >delete Patient Button </td>
 															</tr>
 															
 															
