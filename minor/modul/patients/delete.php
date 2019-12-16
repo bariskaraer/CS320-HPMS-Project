25b1e82cb667876990edb955a45de2dad7120707
MustafaAlperSayan <58241608+MustafaAlperSayan@users.noreply.github.com>
2019-12-16 17:14:04 +0300

    edited patients delete

--- minor/modul/patients/delete.php
+++ minor/modul/patients/delete.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>http   
+<?php	
+	$patientsID = $r3;
+	$query = $db->prepare("DELETE FROM patients where patientsID=?");
+    $delete=$query->execute(array($patientsID));
+    Header ("Location: $url/patients/list");
+	?>
