7d1aa8a1c5480c4ad829d21508f96999cd42bf18
Baris Karaer <bariskaraerx@gmail.com>
2019-12-16 20:57:12 +0300

    Update delete.php

--- minor/modul/patients/delete.php
+++ minor/modul/patients/delete.php
@@ -1,6 +1,6 @@
 <?php	
 	$patientsID = $r3;
-	$query = $db->prepare("DELETE FROM patients where patientsID=?");
+	$query = $db->prepare("DELETE FROM patients where patientID=?");
     $delete=$query->execute(array($patientsID));
     Header ("Location: $url/patients/list");
 	?>
