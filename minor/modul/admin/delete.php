d472ed7b1fac319f152d7d576a1085954464a4a3
MustafaAlperSayan <58241608+MustafaAlperSayan@users.noreply.github.com>
2019-12-16 17:15:00 +0300

    admins delete update

--- minor/modul/admin/delete.php
+++ minor/modul/admin/delete.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>http   
+<?php	
+	$adminID = $r3;
+	$query = $db->prepare("DELETE FROM admins where adminID=?");
+    $delete=$query->execute(array($adminID));
+    Header ("Location: $url/admins/list");
+	?>
