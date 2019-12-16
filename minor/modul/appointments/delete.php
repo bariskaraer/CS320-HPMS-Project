a53d6d8e4f363f33d5920b6ed00a13c671f4b9e5
MustafaAlperSayan <58241608+MustafaAlperSayan@users.noreply.github.com>
2019-12-16 17:09:35 +0300

    added appointments delete

--- minor/modul/appointments/delete.php
+++ minor/modul/appointments/delete.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>http   
+<?php	
+	$doctorID = $r3;
+	$query = $db->prepare("DELETE FROM appointments where appointmentsID=?");
+    $delete=$query->execute(array($appointmentsID));
+    Header ("Location: $url/appointments/list");
+	?>
