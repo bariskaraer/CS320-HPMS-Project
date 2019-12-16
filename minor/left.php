2525e76df92a6b5eaa71c2202d9021923d836061
Baris Karaer <bariskaraerx@gmail.com>
2019-12-16 16:45:11 +0300

    Update left.php

--- minor/left.php
+++ minor/left.php
@@ -15,7 +15,7 @@
 											<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Subheaders</span></span></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/appointments/add" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Add Appointment</span></a></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/appointments/list" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">List Appointments</span></a></li>
-											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/appointments/delete" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Delete Appointments</span></a></li>
+											
 										</ul>
 									</div>
 								</li>
@@ -31,13 +31,13 @@
 									<h4 class="kt-menu__section-text">Patient Actions</h4>
 									<i class="kt-menu__section-icon flaticon-more-v2"></i>
 								</li>
-								<li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-icon"><i class="flaticon-users"></i></span><span class="kt-menu__link-text">Patients </span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
+								<li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-icon"><i class="flaticon-users"></i></span><span class="kt-menu__link-text">Patients </span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
 									<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
 										<ul class="kt-menu__subnav">
 											<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Subheaders</span></span></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/patients/add" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Add Patient</span></a></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/patients/list" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">List Patients</span></a></li>
-											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/patients/delete" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Delete Patients</span></a></li>
+											
 										</ul>
 									</div>
 								</li>	
@@ -55,7 +55,7 @@
 											<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Subheaders</span></span></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/complaints/add" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Add Complaint</span></a></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/complaints/list" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">List Complaints</span></a></li>
-											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/complaints/delete" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Delete Complaints</span></a></li>
+											
 										</ul>
 									</div>
 								</li>
@@ -72,7 +72,7 @@
 											<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">Subheaders</span></span></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/doctors/add" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Add Doctor</span></a></li>
 											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/doctors/list" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">List Doctor</span></a></li>
-											<li class="kt-menu__item " aria-haspopup="true"><a href="<? echo $url ?>/doctors/delete" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Delete Doctor</span></a></li>
+											
 										</ul>
 									</div>
 								</li>
